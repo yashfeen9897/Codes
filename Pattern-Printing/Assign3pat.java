f52312a44c7500651c80e94dce5d5348495e14e8
yashfeen <yashfeenmirza9897@gmail.com>
2017-08-20 12:57:07 +0530

    1    1

---
+++ Pattern-Printing/Assign3pat.java
@@ -0,0 +1,32 @@
+package inputoutput;
+
+import java.util.Scanner;
+
+public class Assign3pat {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		s.close();
+		int i=1,j=1,p;
+		for(i=1;i<=n;i++)
+		{
+			for(j=1;j<=i;j++)
+			{
+				System.out.print(j);
+			}
+			for(j=1;j<=(2*(n-i));j++)
+			{
+				System.out.print(" ");
+			}
+			p=i;
+			for(j=1;j<=i;j++)
+			{
+				System.out.print(p--);
+			}
+				System.out.println();
+		}
+		
+	}
+
+}
